382712cbdf376dd8259722b9911b6674f6195406
takux <53621441+takux@users.noreply.github.com>
2022-10-25 20:12:08 +0900

    0090-create_board-3

--- function_version.py
+++ function_version.py
@@ -1,35 +1,20 @@
-# [[0, 0, 0, 0, 0, 0, 0, 0],
-#  [0, 0, 0, 0, 0, 0, 0, 0],
-#  [0, 0, 0, 0, 0, 0, 0, 0],
-#  [0, 0, 0, 1, 2, 0, 0, 0],
-#  [0, 0, 0, 2, 1, 0, 0, 0],
-#  [0, 0, 0, 0, 0, 0, 0, 0],
-#  [0, 0, 0, 0, 0, 0, 0, 0],
-#  [0, 0, 0, 0, 0, 0, 0, 0]]
+def create_board():
+    """Create initial board."""
+    board = []
+    for r in range(8):
+        board.append([])
+        for c in range(8):
+            cell = {"r": r, "c": c, "state": 0}
+            if r == 3 and c == 3:
+                cell["state"] = 1
+            elif r == 3 and c == 4:
+                cell["state"] = 2
+            elif r == 4 and c == 3:
+                cell["state"] = 2
+            elif r == 4 and c == 4:
+                cell["state"] = 1
+            board[r].append(cell)
+    return board
 
 
-# board (8x8)
-board = []
-
-# 8 rows
-for r in range(8):
-    # print(r)
-    board.append([])
-
-    # 8 columns
-    for c in range(8):
-
-        # cell
-        if r == 3 and c == 3:
-            board[r].append(1)
-        elif r == 3 and c == 4:
-            board[r].append(2)
-        elif r == 4 and c == 3:
-            board[r].append(2)
-        elif r == 4 and c == 4:
-            board[r].append(1)
-        else:
-            board[r].append(0)
-
-
-board
+initial_board = create_board()
